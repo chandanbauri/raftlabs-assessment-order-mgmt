86682cf5fcff0a990f5e0bad880633b16944f462
chandan bauri <chandanbauri441@gmail.com>
2026-02-24 02:20:49 +0530

    feat: Update database connection to use Supabase credentials and dynamically configure SSL mode.

--- backend/database/db.go
+++ backend/database/db.go
@@ -18,12 +18,13 @@ func InitDB() {
 		log.Println("No .env file found")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable search_path=%s",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s search_path=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
 		os.Getenv("DB_SCHEMA"),
 	)
 
